843d227de477a0952cd19e2bffbb9bbe353d0b58
David Wheatley <david@davwheat.dev>
2022-10-22 21:41:34 +0100

    chore: initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mobile Combos API</title>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+        }
+
+        main {
+            padding: 32px;
+        }
+    </style>
+</head>
+
+<body>
+    <main>
+        <h1>Mobile Combos API</h1>
+
+        <p>
+            There's nothing to see here! Did you mean to visit <a href="https://mobilecombos.com">mobilecombos.com</a>?
+        </p>
+    </main>
+</body>
+
+</html>
