2900405e885aa5792ca786699ffbac3b9977721d
David Wheatley <david@davwheat.dev>
2022-10-24 04:26:16 +0100

    feat: add basic DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('modems')->insert([
+            [
+                'uuid' => Uuid::uuid4(),
+                'modem_name' => 'Shannon 5300',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ], [
+                'uuid' => Uuid::uuid4(),
+                'modem_name' => 'Shannon 5123b',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('devices')->insert([
+            [
+                'uuid' => Uuid::uuid4(),
+                'device_name' => 'Pixel 6 Pro',
+                'model_name' => 'GLU0G',
+                'manufacturer' => 'Google',
+                'modem_id' => 2,
+                'release_date' => '2021-10-28',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'uuid' => Uuid::uuid4(),
+                'device_name' => 'Pixel 7 Pro',
+                'model_name' => 'GP4BC',
+                'manufacturer' => 'Google',
+                'modem_id' => 1,
+                'release_date' => '2022-10-13',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
