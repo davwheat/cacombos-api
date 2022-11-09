971de3da63d0fd445400f6b03dc7e99d91218001
David Wheatley <david@davwheat.dev>
2022-11-09 09:06:02 +0000

    feat: add trait for uuids

---
+++ app/Models/Traits/HasSecondaryUuid.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\Traits;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use \Illuminate\Support\Str;
+
+trait HasSecondaryUuid
+{
+    public function scopeUuid(QueryBuilder $query, string $uuid): QueryBuilder
+    {
+        return $query->where($this->getUuidName(), $uuid);
+    }
+
+    public function getUuidName(): string
+    {
+        return property_exists($this, 'uuidName') ? $this->uuidName : 'uuid';
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getUuidName()} = Str::orderedUuid();
+        });
+    }
+}
