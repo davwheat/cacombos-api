29b87aad7b756782752165b1fac3c564933726e3
David Wheatley <david@davwheat.dev>
2023-02-07 19:32:59 +0000

    feat: add File validator rule; add max size param

--- app/Rules/File.php
+++ app/Rules/File.php
@@ -5,10 +5,13 @@ namespace App\Rules;
 use Illuminate\Contracts\Validation\InvokableRule;
 use Psr\Http\Message\UploadedFileInterface;
 
-class FileOrString implements InvokableRule
+class File implements InvokableRule
 {
     private int $maxSize;
 
+    /**
+     * @param integer $maxSize Maximum size in bytes
+     */
     public function __construct(int $maxSize = -1)
     {
         $this->maxSize = $maxSize;
@@ -22,17 +25,15 @@ class FileOrString implements InvokableRule
      */
     public function __invoke($attribute,  $value, $fail)
     {
-        if (!is_string($value) && !($value instanceof UploadedFileInterface)) {
+        if (!($value instanceof UploadedFileInterface)) {
             $fail('The :attribute must either be a string or file.');
         }
 
-        if (($value instanceof UploadedFileInterface)) {
-            /**
-             * @var UploadedFileInterface $value
-             */
-            if ($this->maxSize > 0 && $value->getSize() > $this->maxSize) {
-                $fail('The :attribute must be less than ' . $this->maxSize . ' bytes.');
-            }
+        /**
+         * @var UploadedFileInterface $value
+         */
+        if ($this->maxSize > 0 && $value->getSize() > $this->maxSize) {
+            $fail('The :attribute must be less than ' . $this->maxSize . ' bytes.');
         }
     }
 }
