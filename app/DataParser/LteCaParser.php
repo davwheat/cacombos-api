221c6118d3620a6b5b104ec06ff4e0abefc3361a
David Wheatley <david@davwheat.dev>
2023-06-22 01:08:07 +0100

    wip: more wip parsing

---
+++ app/DataParser/LteCaParser.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\DataParser;
+
+use Illuminate\Database\Eloquent\Collection;
+
+use App\Models\Combo;
+
+class LteCaParser
+{
+    private array $data;
+
+    public function __construct(array $lteCaData)
+    {
+        $this->data = $lteCaData;
+    }
+
+    public function parseToModels(): Collection
+    {
+        $collection = new Collection();
+
+        foreach ($this->data as $lteCa) {
+            $collection->push($this->parseLteCa($lteCa));
+        }
+    }
+
+    protected function parseLteCa(array $combo): Collection
+    {
+        $collection = new Collection();
+
+        $combo = Combo::firstOrCreate([
+            'band' => $combo['band'],
+        ]);
+
+        foreach ($combo['components'] as $lteCa) {
+            $collection->push($this->parseLteCaComponent($lteCa));
+        }
+
+        return $collection;
+    }
+
+    protected function lteCaToComboString(array $combo): string
+    {
+        $comboStringComponents = [];
+
+        foreach ($combo['components'] as $lteCa) {
+            $component = $lteCa['band'];
+
+            if (isset($lteCa['bwClassDl'])) {
+                $component .= $lteCa['bwClassDl'];
+            }
+
+            if (isset($lteCa['mimoDl'])) {
+                switch ($lteCa['mimoDl']['type']) {
+                    case 'single':
+                        $component .= $lteCa['mimoDl']['type']['value'];
+                        break;
+
+                    case 'mixed':
+                        /** @var array */
+                        $allValues = $lteCa['mimoDl']['type']['value'];
+                        $component .= max($allValues);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            if (isset($lteCa['bwClassUl'])) {
+                $component .= $lteCa['bwClassUl'];
+            }
+
+            $comboStringComponents[] = $component;
+        }
+
+        $comboString = implode('-', $comboStringComponents);
+
+        return $comboString;
+    }
+}
